cf8868660fdde65c3c04d0c8c4389a63345effaf
Mister Cool Kid <nyoung22@midviewk12.org>
2020-03-04 17:44:21 -0500

    adwswd

---
+++ Week3CodeAlong/Week3CodeAlong/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Week3CodeAlong
+{
+    class Dog
+    {
+        public string breed;
+        public int height;
+        public string name;
+
+        public void Bark()
+        {
+            MessageBox.Show("Bork!");
+        }
+
+        public void SayName()
+        {
+            
+            MessageBox.Show("My Name is " + name);
+        }
+    }
+}
